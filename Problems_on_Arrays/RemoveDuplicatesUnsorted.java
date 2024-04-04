533325e783e336529e535a96f7cdbc0b4c444334
subrat yadav <96230177+subrat937@users.noreply.github.com>
2024-04-04 21:50:04 +0530

    Update RemoveDuplicatesUnsorted.java

--- Problems_on_Arrays/RemoveDuplicatesUnsorted.java
+++ Problems_on_Arrays/RemoveDuplicatesUnsorted.java
@@ -8,29 +8,15 @@ public class RemoveDuplicatesUnsorted {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the number of elements:");
-        int n=sc.nextInt();
-        int[] arr=new int[n];
+        int n=s.nextInt();
+        HashSet <Integer> st=new HashSet<>();
+       
         for (int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+            set.add(s.nextInt());
         }
         //storing unique elements and adding it to List 
-        HashMap<Integer,Integer> map=new HashMap<>();
-        
-        for (int i : arr){
-            if (!map.containsKey(i)){
-                map.put(i,1);
-                
-            }
-        }
-        int[] res=new int[map.size()];
-        int counter=0;
-        for (int i:map.keySet()){
-            res[counter]=i;
-            counter++;
-        }
-        //displaying the output array
-        for (int i=0;i<map.size();i++){
-            System.out.print(res[i]+" ");
+        for(int i:st){
+          system.out.print(i);
         }
         sc.close();
 
